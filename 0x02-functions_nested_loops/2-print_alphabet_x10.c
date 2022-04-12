00c98fbd8d56098873bbb1f169f845476518afcd
kiru19 <kirubels1994@gmail.com>
2022-04-12 13:36:52 -0700

    2t

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 	char ch;
 	int i;
 
-	i= 0;
+	i = 0;
 
 	while (i < 10)
 	{
@@ -20,7 +20,6 @@ void print_alphabet_x10(void)
 			ch++;
 		}
 
-	       
 		_putchar('\n');
 		i++;
 	}
