1a8fe1c7d4889591bf10b10808f4093bfb858e00
kiru19 <kirubels1994@gmail.com>
2022-04-18 13:23:17 -0700

    5t

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ */
+void rev_string(char *s)
+{
+	char tmp;
+	int i, kiru, kiru1;
+
+	kiru = 0;
+	kiru1 = 0;
+
+	while (s[kiru] != '\0')
+	{
+		kiru++;
+	}
+
+	kiru1 = kiru - 1;
+	
+	for (i = 0; i < kiru / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[kiru1];
+		s[kiru1--] = tmp;
+	}
+}
