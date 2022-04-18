3db3d9705ce5e4cf69fdd4c3e5788fe34e26be3a
kiru19 <kirubels1994@gmail.com>
2022-04-18 13:41:35 -0700

    7t

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ *followed by a new line
+ * @str: string to be printed
+ */
+void puts_half(char *str)
+{
+	int kiru, n, i;
+
+	kiru = 0;
+
+	while (str[kiru] != '\0')
+	{
+		kiru++;
+	}
+
+	if (kiru % 2 == 0)
+	{
+		for (i = hab /2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	} else if (kiru % 2)
+	{
+		for (n = (kiru - 1) / 2; n < kiru - 1; n++)
+		{
+			_putchar(str[n + 1]);
+		}
+	}
+	_putchar('\n');
+}
