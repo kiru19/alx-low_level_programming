f9e6cf7695524d9a4eb254221d525d4fc86d36cc
kiru19 <kirubels1994@gmail.com>
2022-04-18 12:46:48 -0700

    4t

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,17 +6,17 @@
  */
 void print_rev(char *s)
 {
-	int i, j, kiru;   
-        i = 0;
-        while (s[i] != '\0')
-	{
-		i++;
-	}
-        kiru = i;
-        for (j = kiru - 1; j >= 0; j--)
-	{
-		_putchar(s[j]);
-	}
+int i, j, kiru;   
+i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+kiru = i;
+for (j = kiru - 1; j >= 0; j--)
+{
+	_putchar(s[j]);
+}
 
 	_putchar('\n');
 }
