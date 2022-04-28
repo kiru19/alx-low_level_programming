c641b225b46da9d0ef8cc70f53c60bd62333421b
kiru19 <kirubels1994@gmail.com>
2022-04-28 00:43:39 -0700

    101t

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -23,6 +23,14 @@ int wildcmp(char *s1, char *s2)
         * if we reach the end of s1 and the char in s2 is a *
         * and if the next chars of s2 are *, return 1
 	*/
+	if (*s1 == '\0' && *s2 == '*' && !*move(s2))
+		return (1);
+	
+	/**
+	 * if the chars are equal in both strings,
+	 * if we reached the end of s1, return 1
+	 * else increment s1 and s2 by 1
+	 */
 	if (*s1 == *s2)
 	{
 		if (*s1 == '\0')
